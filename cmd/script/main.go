953a83c5172822153a47dda69415c6f9f1a3740a
Krisnata <krisnata.panji@gmail.com>
2025-06-10 23:47:21 +0700

    feat: add database drop and seed scripts

--- cmd/script/main.go
+++ cmd/script/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"dealls-case-study/internal/db"
+	"dealls-case-study/internal/seed"
 
 	"github.com/joho/godotenv"
 )
@@ -11,8 +13,33 @@ import (
 func main() {
 	_ = godotenv.Load()
 
-	err := db.CreateDatabase()
-	if err != nil {
-		log.Fatalf("failed creating db: %v", err)
+	opt := os.Args[1]
+
+	switch opt {
+	case "create":
+		{
+			err := db.CreateDatabase()
+			if err != nil {
+				log.Fatalf("failed creating db: %v", err)
+			}
+		}
+
+	case "drop":
+		{
+			err := db.DropDatabase()
+			if err != nil {
+				log.Fatalf("failed dropping db: %v", err)
+			}
+		}
+
+	case "seed":
+		{
+			db.InitDB()
+			err := seed.Run(db.DB)
+			if err != nil {
+				log.Fatalf("failed seeding data: %v", err)
+			}
+		}
 	}
+
 }
--- internal/seed/seed.go
+++ internal/seed/seed.go
@@ -2,15 +2,18 @@ package seed
 
 import (
 	"dealls-case-study/internal/models"
+	"dealls-case-study/internal/utils"
+	"fmt"
 	"log"
 
 	"gorm.io/gorm"
 )
 
 func Run(db *gorm.DB) error {
+	log.Println("Seeding data to database...")
 	roles := []models.Role{
-		{Name: "Admin"},
-		{Name: "Employee"},
+		{Name: "Admin", CreatedBy: 999},
+		{Name: "Employee", CreatedBy: 999},
 	}
 
 	// seed roles
@@ -22,7 +25,51 @@ func Run(db *gorm.DB) error {
 		}
 	}
 
-	
+	// seed admin
+	password, err := utils.HashPassword("password")
+	if err != nil {
+		log.Printf("Failed creating admin password: %v", err)
+		return err
+	}
+	admin := models.User{
+		ID:       999,
+		Username: "admin",
+		Password: password,
+		RoleID:   1,
+	}
+	err = db.FirstOrCreate(&admin, models.User{ID: admin.ID}).Error
+	if err != nil {
+		log.Printf("Failed to seed admin: %v", err)
+		return err
+	}
+
+	// seed users
+	var users = []models.User{}
+	for i := uint(1); i <= 100; i++ {
+		username := fmt.Sprintf("%s%d", "user", i)
+
+		if err != nil {
+			log.Printf("Failed to seed user %s: %v", username, err)
+			return err
+		}
+
+		user := models.User{
+			ID:        i,
+			Username:  username,
+			Password:  password,
+			Salary:    float64(i * 10),
+			RoleID:    uint(2),
+			CreatedBy: 999,
+		}
+		users = append(users, user)
+	}
+
+	err = db.Create(&users).Error
+	if err != nil {
+		log.Println("Failed to seed users: ", err)
+		return err
+	}
+	log.Println("Seeding completed!")
 
 	return nil
 }
