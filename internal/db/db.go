43393017190c8d462df99c00b4c0efbaff5202ed
Krisnata <krisnata.panji@gmail.com>
2025-06-11 09:16:56 +0700

    feat: add testing libraries

--- internal/db/db.go
+++ internal/db/db.go
@@ -1,12 +1,16 @@
 package db
 
 import (
+	"context"
 	"dealls-case-study/internal/models"
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/go-gormigrate/gormigrate/v2"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -63,4 +67,64 @@ func RunMigrations(db *gorm.DB) error {
 	})
 
 	return m.Migrate()
+}
+
+func InitTestDB() (*gorm.DB, func(), error) {
+	ctx := context.Background()
+
+	req := testcontainers.ContainerRequest{
+		Image:        "postgres:latest",
+		ExposedPorts: []string{"5432/tcp"},
+		Env: map[string]string{
+			"POSTGRES_PASSWORD": "password",
+			"POSTGRES_DB":       "testdb",
+		},
+		WaitingFor: wait.ForListeningPort("5432/tcp").WithStartupTimeout(60 * time.Second),
+	}
+
+	postgresC, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+	if err != nil {
+		// panic(err)
+		return nil, nil, err
+	}
+
+	host, _ := postgresC.Host(ctx)
+	port, _ := postgresC.MappedPort(ctx, "5432")
+
+	dsn := fmt.Sprintf("host=%s port=%s user=postgres password=password dbname=testdb sslmode=disable", host, port.Port())
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		// panic("failed to connect to database")
+		return nil, nil, err
+	}
+
+	db.AutoMigrate(&models.Attendance{}, &models.Overtime{}, &models.Payroll{}, &models.Payslip{}, &models.Reimbursement{}, &models.Role{}, &models.User{})
+
+	DB = db
+
+	roles := []models.Role{
+		{Name: "Admin", CreatedBy: 999},
+		{Name: "Employee", CreatedBy: 999},
+	}
+
+	// seed roles
+	for _, role := range roles {
+		err := db.FirstOrCreate(&role, models.Role{Name: role.Name}).Error
+		if err != nil {
+			log.Printf("Failed to seed role %s: %v", role.Name, err)
+			return nil, nil, err
+		}
+	}
+
+	// Tear down the container after tests
+	// defer postgresC.Terminate(ctx)
+	cleanup := func() {
+		postgresC.Terminate(ctx)
+	}
+
+	return db, cleanup, nil
 }
